4610a33c51c91f347750b60a8140df777b7ee130
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-05-27 20:35:01 +0530

    Add the frontend

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Appbar from '../components/Appbar';
+
+function Home() {
+    return (
+        <div>
+            <Appbar />
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
